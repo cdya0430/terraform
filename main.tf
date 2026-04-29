bf6e5fbde9e6aa2f9fdcb39abfe0281954a0a89f
cdya0430 <dayeonchoe141@gmail.com>
2026-04-29 15:18:00 +0900

    첫 업로드

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
-variable "server_name" {} # 외부에서 이름을 받아옴
-
-resource "local_file" "web_server_info" {
-  filename = "${path.module}/../../${var.server_name}.txt" # 파일 형태로 서버 생성 시뮬레이션
-  content  = "Welcome to ${var.server_name}!"
+module "my_web_servers" {
+  source   = "./modules/web-server"
+  
+  # 변수 파일의 web_servers 맵을 돌면서 하나씩 꺼내옴
+  for_each = var.web_servers
+  
+  # 모듈 내부의 server_name 변수에 Map의 Key값(서버 이름)을 전달
+  server_name = each.key
 }
